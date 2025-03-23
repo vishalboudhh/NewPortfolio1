69ffcc07022729b3e565707182e2e000ac0e3b4c
vishal meshram <107854847+vishalboudhh@users.noreply.github.com>
2025-03-23 22:04:18 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
         <div className="mt-6">
           <a
             className="px-6 py-3 bg-cyan-600 text-white font-semibold rounded-lg shadow-md hover:bg-cyan-700 transition-all"
-            href="https://drive.google.com/file/d/1MClQFJ3KwbV4VCZAzwq8wXI23Sy27cGe/view?usp=sharing"
+            href="https://drive.google.com/file/d/1rv27mzq18x2b5m1sPhDQjppTU0NQGbYV/view?usp=sharing"
             download
           >
             Download my CV
